b3638de35eeeb3e39926ccad81f0a39128bd3b13
Ryan Dsilva <ryan.dsilva.98@gmail.com>
2019-03-31 02:59:49 +0530

    full cards

---
+++ components/Workout/fullWidthCard.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { View, Text, ImageBackground, Image } from "react-native";
+
+export default class FullWidthCard extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+  }
+  render() {
+    return (
+      <View
+        style={{
+          ...styles.cardMainBody,
+          height: 220
+        }}
+      >
+        <ImageBackground
+          source={this.props.bgImg}
+          imageStyle={{ borderRadius: 15 }}
+          style={{ width: "100%", height: "100%" }}
+        >
+          <View style={{ flexDirection: "row", height: "100%" }}>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "column",
+                justifyContent: "flex-end"
+              }}
+            >
+              <View style={{ flex: 0, margin: 20 }}>
+                <Text
+                  style={{ fontSize: 27, color: "white", fontWeight: "bold" }}
+                >
+                  {this.props.workoutType}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 21,
+                    color: "white",
+                    marginBottom: 9
+                  }}
+                >
+                  Get Started
+                </Text>
+                <View
+                  style={{
+                    backgroundColor: "white",
+                    borderRadius: 20,
+                    color: "blue",
+                    padding: 7,
+                    flex: 0,
+                    width: 105
+                  }}
+                >
+                  <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+                    Start Now
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                alignSelf: "flex-start",
+                justifyContent: "flex-end",
+                flexDirection: "row",
+                justifyContent: "flex-end"
+              }}
+            >
+              <Image
+                source={this.props.person}
+                style={{ height: 80, width: 40, margin: 13 }}
+              />
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
+styles = {
+  cardMainBody: {
+    margin: 15
+  }
+};
--- components/Workout/index.js
+++ components/Workout/index.js
@@ -1,12 +1,48 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import FullWidthCard from "./fullWidthCard";
+
+import bg_0 from "../../assets/images/card-bg/workout_recent_1.png";
+import bg_1 from "../../assets/images/card-bg/workout_recent_1a.png";
+import bg_2 from "../../assets/images/card-bg/workout_recent_2a.png";
+import bg_3 from "../../assets/images/card-bg/workout_recent_3a.png";
+import bg_4 from "../../assets/images/card-bg/workout_recent_4a.png";
+import bg_5 from "../../assets/images/card-bg/workout_recommendeda.png";
+
+import fullbody_person from "../../assets/images/card-bg/icon_workout_white_fullbody.png";
+import aerobic_person from "../../assets/images/card-bg/person/icon_workout_white_aerobic.png";
+import arms_person from "../../assets/images/card-bg/person/icon_workout_white_arms.png";
+import athletic_person from "../../assets/images/card-bg/person/icon_workout_white_athletic.png";
+import backandcore_person from "../../assets/images/card-bg/person/icon_workout_white_backandcore.png";
+import extremecardio_person from "../../assets/images/card-bg/person/icon_workout_white_extremecardio.png";
+import fatburn_person from "../../assets/images/card-bg/person/icon_workout_white_fatburn.png";
+import yogalates_person from "../../assets/images/card-bg/person/icon_workout_white_yogalates.png";
+import yogastretch_person from "../../assets/images/card-bg/person/icon_workout_white_yogastretch.png";
+
+var data = [
+  { bg: bg_0, person: fullbody_person, name: "Full body" },
+  { bg: bg_1, person: aerobic_person, name: "Aerobic" },
+  { bg: bg_3, person: athletic_person, name: "Athletic" },
+  { bg: bg_4, person: backandcore_person, name: "Back and Core" },
+  { bg: bg_5, person: extremecardio_person, name: "Cardio" },
+  { bg: bg_2, person: arms_person, name: "Arms" },
+  { bg: bg_3, person: fatburn_person, name: "Fatburn" },
+  { bg: bg_2, person: yogalates_person, name: "Yoga" }
+];
 
 export default class Workout extends React.Component {
   render() {
-    return (
-      <View>
-        <Text>Workout</Text>
-      </View>
-    );
+    let g = [];
+    for (let i = 0; i < data.length; i++) {
+      g.push(
+        <FullWidthCard
+          workoutType={data[i].name}
+          navPoint={data[i]}
+          bgImg={data[i].bg}
+          person={data[i].person}
+        />
+      );
+    }
+    return <ScrollView>{g}</ScrollView>;
   }
 }
