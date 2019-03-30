4d2ec3c5932cd1b7af66423a44800e450e0b23c4
Ryan Dsilva <ryan.dsilva.98@gmail.com>
2019-03-31 04:23:31 +0530

    added more moves

--- components/Exercise/index.js
+++ components/Exercise/index.js
@@ -5,18 +5,24 @@ import gif_1 from "../../assets/images/card-bg/Exercises/1_M.gif";
 import gif_2 from "../../assets/images/card-bg/Exercises/2_M.gif";
 import gif_3 from "../../assets/images/card-bg/Exercises/3_M.gif";
 import gif_4 from "../../assets/images/card-bg/Exercises/4_M.gif";
-// import gif_1 from "../../assets/images/card-bg/Exercises/1_M.gif";
-// import gif_1 from "../../assets/images/card-bg/Exercises/1_M.gif";
-// import gif_1 from "../../assets/images/card-bg/Exercises/1_M.gif";
-// import gif_1 from "../../assets/images/card-bg/Exercises/1_M.gif";
-// import gif_1 from "../../assets/images/card-bg/Exercises/1_M.gif";
-// import gif_1 from "../../assets/images/card-bg/Exercises/1_M.gif";
+import gif_5 from "../../assets/images/card-bg/Exercises/1_M.gif";
+import gif_6 from "../../assets/images/card-bg/Exercises/1_M.gif";
+import gif_7 from "../../assets/images/card-bg/Exercises/1_M.gif";
+import gif_8 from "../../assets/images/card-bg/Exercises/1_M.gif";
+import gif_9 from "../../assets/images/card-bg/Exercises/1_M.gif";
+import gif_10 from "../../assets/images/card-bg/Exercises/1_M.gif";
 
 var exerciseData = [
   { img: gif_1, name: "Jumping Jack" },
-  { img: gif_2, name: "Wall Sit" },
   { img: gif_3, name: "Push Up" },
-  { img: gif_4, name: "Crunches" }
+  { img: gif_2, name: "Wall Sit" },
+  { img: gif_4, name: "Crunches" },
+  { img: gif_5, name: "Steps" },
+  { img: gif_6, name: "Squat" },
+  { img: gif_7, name: "Triceps Push Up" },
+  { img: gif_8, name: "Plank" },
+  { img: gif_9, name: "Jog on Spot" },
+  { img: gif_10, name: "Lunges" }
 ];
 
 class Exercise extends Component {
@@ -33,7 +39,7 @@ class Exercise extends Component {
 export default class ExercisesPage extends React.Component {
   render() {
     let g = [];
-    for (let i = 0; i < exerciseData.length; i++) {
+    for (let i = 0; i < 2; i++) {
       g.push(<Exercise index={i} />);
     }
     return <ScrollView>{g}</ScrollView>;
