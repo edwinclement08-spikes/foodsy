00848f7aa9b9918c5597af634d531d6600fc3902
Nehal Kalnad <yenwiikae@gmail.com>
2019-03-31 07:08:52 +0530

    auth

--- components/Login/register.js
+++ components/Login/register.js
@@ -1,240 +1,119 @@
-import React, {Component} from "react";
-import {StyleSheet, ImageBackground, Image, View, Dimensions, PermissionsAndroid} from 'react-native';
+import React, { Component } from 'react';
 import {
-    Container, Content, Button, Item, Label, Input, Form,Radio,Text, Icon
-} from "native-base";
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import logo from '../../assets/images/logo.png';
-import ImagePicker from 'react-native-image-picker';
-import axios from 'axios';
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Image,
+  Alert
+} from 'react-native';
+import axios from "axios"
 
-const {width: WIDTH} = Dimensions.get('window');
-export default class SignIn extends Component {
-    constructor(props)  {
-        super(props);
-        this.state = {
-            errorMessage: false,
-            drivOpt:'1',
-            fd : null,
-            licenceUrl : '',
-            imageUrl : ''
-            
-        }
+export default class LoginView extends Component {
+
+  constructor(props) {
+    super(props);
+    state = {
+      email   : '',
+      password: '',
+      name: ''
     }
+  }
 
-    async componentDidMount(){
+  register = () => {
+    axios.post('http://foodsy-unscript.herokuapp.com/register', 
+    {   
+        name: this.state.name, 
+        email: this.state.email, 
+        password: this.state.password
+    })
+    .then(res => {
+        Alert.alert("Success", "User Registered Successfully")
+    })
+  }
 
-        console.log("mounting")
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.CAMERA,
-          {
-            title: 'Cool Photo App Camera Permission',
-            message:
-              'Cool Photo App needs access to your camera ' +
-              'so you can take awesome pictures.',
-            buttonNeutral: 'Ask Me Later',
-            buttonNegative: 'Cancel',
-            buttonPositive: 'OK',
-          },
-        );
-    
-        console.log(granted)        
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/people/3495db'}}/>
+          <TextInput style={styles.inputs}
+              placeholder="Name"
+              underlineColorAndroid='transparent'
+              onChangeText={(name) => this.setState({name})}/>
+        </View>
 
-    }
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/message/ultraviolet/50/3498db'}}/>
+          <TextInput style={styles.inputs}
+              placeholder="Email"
+              keyboardType="email-address"
+              underlineColorAndroid='transparent'
+              onChangeText={(email) => this.setState({email})}/>
+        </View>
+        
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db'}}/>
+          <TextInput style={styles.inputs}
+              placeholder="Password"
+              secureTextEntry={true}
+              underlineColorAndroid='transparent'
+              onChangeText={(password) => this.setState({password})}/>
+        </View>
 
-
-    uploadDrivingLicence = async () =>{
-
-        var fd = new FormData();
-        ImagePicker.launchImageLibrary({}, (response) => {
-      
-      
-
-            fd.append('file', {
-              uri: response.uri,
-              type: response.type,
-              name: response.fileName
-            });
-      
-            this.setState({ fd })
-            // console.log(fd)
-
-            var ref = this;
-            var url = 'http://192.168.1.108:3000/upload';
-            axios({
-              method: 'post',
-              url: url,
-              data: fd,
-              config: { headers: {'Content-Type': 'multipart/form-data' }}
-              })
-              .then(function (response) {
-                  //handle success
-                  var data = response.data;
-                  ref.setState({ licenceUrl : data.url })
-                  console.log( ref.state.licenceUrl );
-              })
-              .catch(function (response) {
-                  //handle error
-                  console.log(response);
-            });
-      
-          });
-
-
-
-    }
-
-    uploadProfilePicture = () =>{
-
-        var fd = new FormData();
-        ImagePicker.launchImageLibrary({}, (response) => {
-      
-      
-
-            fd.append('file', {
-              uri: response.uri,
-              type: response.type,
-              name: response.fileName
-            });
-      
-            this.setState({ fd })
-            // console.log(fd)
-
-            var ref = this;
-            var url = 'http://192.168.1.108:3000/upload';
-            axios({
-              method: 'post',
-              url: url,
-              data: fd,
-              config: { headers: {'Content-Type': 'multipart/form-data' }}
-              })
-              .then(function (response) {
-                  //handle success
-                  var data = response.data;
-                  ref.setState({ imageUrl : data.url })
-                  console.log();
-              })
-              .catch(function (response) {
-                  //handle error
-                  console.log(response);
-            });
-      
-          });
-
-    }
-
-
-
-    render() {
-        return (
-            <Container>
-                <Content>
-
-                    <View style={styles.logoContainer}>
-                        <Text style={styles.logoText}>Registration</Text>
-                    </View>
-
-                    <View style={{paddingLeft: 20, paddingRight: 20}}>
-                        {this.state.errorMessage &&
-                        <View style={{backgroundColor:"lightpink", padding:20,
-                         borderRadius:10, borderWidth:2, borderColour:"red", textAlign:"center", marginTop:20}}>
-                            <Text>{this.state.errorMessage}</Text>
-                        </View> }
-                        
-                        <Form block style={styles.item}>
-                            <Item block floatingLabel>
-                                <Label block style={{marginBottom: 20}}>
-                                    <Text>Name</Text>
-                                </Label>
-                                <Input block
-                                       onChangeText={(text) => this.setState({"formEmail":text})}
-                                       value={this.state["formEmail"]} />
-                            </Item>
-                            <Item floatingLabel>
-                                <Label>Email</Label>
-                                <Input secureTextEntry
-                                       onChangeText={(text) => this.setState({"formPassword":text})}
-                                       value={this.state["formPassword"]}/>
-                            </Item>
-                            <Item floatingLabel>
-                                <Label>Age</Label>
-                                <Input secureTextEntry
-                                       onChangeText={(text) => this.setState({"formPassword":text})}
-                                       value={this.state["formPassword"]}/>
-                            </Item>
-                            <Item floatingLabel>
-                                <Label>Password</Label>
-                                <Input secureTextEntry
-                                       onChangeText={(text) => this.setState({"formPassword":text})}
-                                       value={this.state["formPassword"]}/>
-                            </Item>
-
-                            <Button rounded info onPress={ this.uploadProfilePicture } style={{textAlign:'center',justifyContent:'center',width:260 ,marginTop: 30, alignSelf: 'center', backgroundColor:"#0083d9"}}>
-                                <Text>Select Profile Image</Text>
-                            </Button>
-                            <Button rounded info onPress={ this.uploadDrivingLicence } style={{textAlign:'center',justifyContent:'center',width:260 ,marginTop: 30, alignSelf: 'center', backgroundColor:"#0083d9"}}>
-                                <Text>Select Driving Licence</Text>
-                            </Button>
-
-                            
-                            <View style={{flexDirection:'row' , marginLeft:15, marginTop:25}}>
-
-                            <View style={{flex:0.5}}>
-                              <Radio onPress={() => this.setState({ drivOpt: '1' })} selected={this.state.drivOpt == '1'} />
-                              <Text>New Driver</Text> 
-                            </View>
-
-
-                            <View style={{flex:0.5}}>
-                              <Radio onPress={() => this.setState({ drivOpt: '2' })} selected={this.state.drivOpt == '2'} />
-                              <Text>Exprienced Driver</Text>
-                            </View>                        
-
-
-                            </View>
-
-                        </Form>
-
-                        <Button rounded info style={{textAlign:'center',justifyContent:'center',width:260 ,marginTop: 30, alignSelf: 'center', backgroundColor:"#0083d9"}}>
-                            <Text >Register</Text>
-                        </Button>
-
-
-
-                    </View>
-
-                </Content>
-            </Container>
-        );
-    }
+        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={this.register}>
+          <Text style={styles.loginText}>Register</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    backgroundContainer: {
-        flex: 1,
-        width: null,
-        height: null,
-        justifyContent: 'center',
-        alignItems: 'center',
-
-    },
-    logoContainer: {
-        alignItems: 'center',
-        marginTop: 30
-    },
-    logo: {
-        height: 150,
-        width: 150,
-        resizeMode: 'contain'
-    },
-    logoText: {
-        color: 'black',
-        fontSize: 30,
-        fontWeight: '300',
-        opacity: 0.9,
-    },
-    item: {
-        paddingTop: 10,
-    },
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCDCDC',
+  },
+  inputContainer: {
+      borderBottomColor: '#F5FCFF',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:250,
+      height:45,
+      marginBottom:20,
+      flexDirection: 'row',
+      alignItems:'center'
+  },
+  inputs:{
+      height:45,
+      marginLeft:16,
+      borderBottomColor: '#FFFFFF',
+      flex:1,
+  },
+  inputIcon:{
+    width:30,
+    height:30,
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+  },
+  loginButton: {
+    backgroundColor: "#00b5ec",
+  },
+  loginText: {
+    color: 'white',
+  }
 });
